920f7963eaf2af0e0586bdd06a73188c256e2658
Sihe Zhang <liubing1@msu.edu>
2018-04-03 14:53:49 -0400

    second attempt

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+var current = document.getElementById('timeTheme')
+var setTheme = window.localStorage.getItem('theme') || 'day'
+function daythemechange () {
+  if (current.className === 'day') {
+    current.setAttribute('class', 'night')
+    setTheme = 'night'
+    window.localStorage.setItem('theme', setTheme)
+  } else {
+    current.setAttribute('class', 'day')
+    setTheme = 'day'
+    window.localstorage.setItem('theme', setTheme)
+  }
+}
 
-var Daytheme = document.getElementById('day')
-var Nighttheme = document.getElementById('night')
+window.onload = function () {
+  document.getElementById('button').addEventListener('click', daythemechange)
+  current.setAttribute('class', setTheme)
+}
+
+window.localStorage.setItem('theme', setTheme)
+
 var quantity = parseInt(window.localStorage.getItem('quantity'))
 var greeting = document.getElementById('greeting')
 
@@ -10,7 +28,4 @@ if (isNaN(quantity)) {
   quantity = quantity + 1
 }
 window.localStorage.setItem('quantity', quantity)
-greeting.textContent = 'quantity: ' + quantity
-
-function daythemechange () {  
-}
+greeting.textContent = 'You have visited this site ' + quantity + ' times:)'
